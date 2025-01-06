ddece5c479a354674c7a6a5178b202f772adee65
Tenkenoz <epobando1@espe.edu.ec>
2025-01-06 10:59:59 -0500

    FINAL

--- TallerFunciones.js
+++ TallerFunciones.js
@@ -14,23 +14,21 @@ Reto: Agrega validaciones para asegurarte de que los parametros son numeros ante
 Si algun parametro no es un numero, lanzar un error con un mensaje descriptivo    
 */
 function calcularPromedio(a, b, c) {
-    if (typeof a !== "number" || typeof b !== "number" || typeof c !== "number") {
-      console.log("Los parametros del promedio deben ser numeros");
-    } else {
-      return (a + b + c) / 3;
-    }
-  }
-function determinarMayor(a, b) {
-    if (typeof a !== "number" || typeof b !== "number") {
-      console.log("Los parametros del valor mayor deben ser numeros");
-    } else {
-      return (a > b) ? a : b;
-    }
-  }
+  return (a + b + c) / 3;
+}
 
-esPar = x => (typeof x !== "number")? console.log("Los parametros del se par, deben ser numeros") :(x%2==0) ? true : false;
-(function(a,b,c){
-    console.log("Promedio: "+calcularPromedio(a,b,c));
-    console.log("El valor mayor es:"+determinarMayor(a,b));
-    console.log("Es par: "+esPar(a));
-})(4,5,8)
+function determinarMayor(a, b) {
+  return (a > b) ? a : b;
+}
+
+esPar = x => (x % 2 == 0) ? true : false;
+
+(function(a, b, c) {
+  if (typeof a !== "number" || typeof b !== "number" || typeof c !== "number") {
+    console.log("Los parámetros deben ser números");
+  } else {
+    console.log("Promedio: " + calcularPromedio(a, b, c));
+    console.log("El valor mayor es: " + determinarMayor(a, b));
+    console.log("Es par: " + esPar(a));
+  }
+})(4, 5, 8);
